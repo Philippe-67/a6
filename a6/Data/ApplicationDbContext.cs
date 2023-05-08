34968ebba8497baab503b2665903aa3ba355e170
Philippe CHARLES <philippe.charles@axa.fr>
2023-05-08 11:53:19 +0200

    Ajoutez des fichiers projet.

---
+++ a6/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using a6.Models;
+
+namespace a6.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<a6.Models.Voiture> Voiture { get; set; } = default!;
+    }
+}
